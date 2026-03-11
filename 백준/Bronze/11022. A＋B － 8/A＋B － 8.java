82caaac89e5205c7ff24b0a6a30c3c43348bfe9e
Byeongwoo An <50216870+ByeongwooAn@users.noreply.github.com>
2026-03-11 23:32:18 +0900

    [Bronze V] Title: A+B - 8, Time: 144 ms, Memory: 16456 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        int A, B;
+
+        StringTokenizer st;
+
+        for(int i = 1; i <= T; i++) {
+            st = new StringTokenizer(br.readLine());
+            A = Integer.parseInt(st.nextToken());
+            B = Integer.parseInt(st.nextToken());
+
+            bw.write("Case #" + i + ": " + A + " + " + B + " = " + (A + B) + "\n");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
