a4f6f8da28e4697db96b10ae0cbfb0166728f18d
Byeongwoo An <50216870+ByeongwooAn@users.noreply.github.com>
2026-03-31 17:58:02 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 708 ms, Memory: 50868 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+        
+        int N = Integer.parseInt(br.readLine());
+        int[][] arrN = new int[N][2];
+
+        for(int n = 0; n < N; n++) {
+            st = new StringTokenizer(br.readLine());
+
+            arrN[n][0] = Integer.parseInt(st.nextToken());
+            arrN[n][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arrN, (a, b) -> {
+            if(a[0] == b[0]) {
+                return Integer.compare(a[1], b[1]);
+            }
+
+            return Integer.compare(a[0], b[0]);
+        });
+
+        for(int i = 0; i < arrN.length; i++) {
+            bw.write(arrN[i][0] + " " + arrN[i][1]);
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
